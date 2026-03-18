3ccddf3a11c131a61df9a25c79f15b0d3c8cef2b
amir-du <amir.dulas@supdevinci-edu.fr>
2026-03-18 14:59:34 +0100

    Update VM configuration with admin password
    Added admin password and disabled password authentication for the VM.

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,8 +113,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size = "Standard_B2als_v2"
+  size                = "Standard_B2als_v2"
   admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  disable_password_authentication = false
   zone                = "1"
 
   network_interface_ids = [
