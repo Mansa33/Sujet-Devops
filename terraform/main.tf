ce1c86ee79cb457f22a882b87000265d7f859430
amir-du <amir.dulas@supdevinci-edu.fr>
2026-03-18 14:23:11 +0100

    Upgrade azurerm provider version to 4.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.85"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id             = var.subscription_id
-  tenant_id                   = var.tenant_id
+  subscription_id                 = var.subscription_id
   resource_provider_registrations = "none"
 }
 
